cc3bdfd0b2ffd14f018e398dcd236106c606d1ff
Thiago Yasunaka <thiagoyasunaka@hotmail.com>
2022-01-24 08:24:16 -0300

    ajusta helpers

---
+++ parallel/src/helpers.h
@@ -0,0 +1,13 @@
+/**********************************************************
+ * Created: Seg 08 Ago 2011 16:20:54 BRT
+ *
+ * Author: Carla N. Lintzmayer, carla0negri@gmail.com
+ * Modified By: Thiago Issao Yasunaka, thiagoyasunaka@hotmail.com
+ **********************************************************/
+
+#ifndef __HELPERS_H
+#define __HELPERS_H
+
+void show_help(char *nameprog);
+
+#endif /* __HELPERS_H */
